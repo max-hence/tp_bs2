94e26ccadf1565d17a39f0669870404886c856fa
max_hence <maxence.brault@gmail.com>
2022-11-30 00:52:16 +0100

    Voilà le git est prêt !

--- testing.py
+++ testing.py
@@ -2,3 +2,10 @@ from graph_tool.all import *
 
 toy_ex = load_graph("toy_example.graphml")
 graph_draw(toy_ex, output_size=(800, 800))
+
+for vertex in toy_ex.vertices():
+    print(f"Voisin du sommet {vertex} : ")
+    for neighbor in vertex.all_neighbors():
+        print('Sommet : ', neighbor)
+
+
