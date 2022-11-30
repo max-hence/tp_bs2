a509d9ac366f072e06cc2877962c8cb3c637d6f4
cbeust <cecile.beust@gmail.com>
2022-11-30 11:35:47 +0100

    ça marche bien

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 from graph_tool.all import *
 
 toy_ex = load_graph("toy_example.graphml")
-graph_draw(toy_ex, output_size=(800, 800))
+graph_draw(toy_ex, vertex_text=toy_ex.vertex_properties["_graphml_vertex_id"], output_size=(800, 800))
 
 for vertex in toy_ex.vertices():
     print(f"Voisin du sommet {vertex} : ")
