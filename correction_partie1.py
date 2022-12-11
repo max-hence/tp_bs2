dbdf0b06efa0cfe1874984f12183ab1eeaf4c46b
max_hence <maxence.brault@gmail.com>
2022-12-11 15:35:56 +0100

    pushpush

--- correction_partie1.py
+++ correction_partie1.py
@@ -3,7 +3,7 @@ Correction partie 1
 """
 
 from graph_tool import GraphView
-from graph_tool.all import graph_draw, load_graph, betweenness, label_out_component
+from graph_tool.all import graph_draw, load_graph, betweenness, label_out_component, find_vertex
 SIZE = 400
 V_SIZE = SIZE/10
 E_PWIDTH = V_SIZE/4
@@ -11,10 +11,10 @@ E_PWIDTH = V_SIZE/4
 # Question 2 : Charger le fichier toy_example.graphml
 g = load_graph("toy_example.graphml")
 g.set_directed(False)
-'''graph_draw(g,
+graph_draw(g,
     vertex_text=g.vertex_properties["_graphml_vertex_id"],
     output_size=(SIZE, SIZE),
-)'''
+)
 
 # Question 3 : Ajouter des sommets
 
@@ -30,7 +30,7 @@ id[v13] = 13
 name[v13] = "P013"
 
 v4 = g.vertex(4)
-print(id[v4]) # quand on fait verted(4) on a le 5e ...
+print(id[v4]) # quand on fait vertex(4) on a le 5e ...
 
 # add edges
 g.add_edge(g.vertex(4), v13)
@@ -48,8 +48,8 @@ for vertex in g.vertices():
 
 
 # Question 5: Colorer les sommets en fonction de la betweenness
-
 bv, be = betweenness(g)
+print([betweenness for betweenness in bv])
 graph_draw(g,
     vertex_fill_color=bv,
     vertex_text=g.vertex_properties["_graphml_vertex_id"],
@@ -57,8 +57,10 @@ graph_draw(g,
 )
 
 
-# Question 6 : Afficher que la composante connexe associée au sommet 4
-l = label_out_component(g, g.vertex(4))
+# Question 6 : Afficher que la composante connexe associée au sommet Expression of APOA1
+
+vertex = find_vertex(g=g, prop=g.vertex_properties["name"], match="assembly of PLCG1:Ca2+")[0]
+l = label_out_component(g, vertex)
 print(l.a)
 u = GraphView(g, vfilt=l)
 graph_draw(u,
